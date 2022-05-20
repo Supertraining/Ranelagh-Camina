becbe7da2fd798ae67bbc8f2d0b0815b04499c40
Matías Maranga <marangamatias@gmail.com>
2022-05-19 21:03:23 -0300

    RanelaghCamina

---
+++ RCam.js
@@ -0,0 +1,8 @@
+function cambiarTexto () {
+    document.getElementById ("prueba").innerHTML = "aprendiendo una nueva habilidad";
+}
+
+function cambiarTexto1 () {
+    document.getElementById("loquito").innerHTML = "Sos un puto crack";
+    document.getElementById("loquito1").innerHTML = "esto es un block code y va entre {}";
+}
