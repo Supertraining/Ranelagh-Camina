47ae3242e878f6fd2a3b3701cdf1503223dc60dd
Matías Maranga <marangamatias@gmail.com>
2022-05-23 00:06:12 -0300

    reformaContacto

--- RCam.js
+++ RCam.js
@@ -6,3 +6,4 @@ function cambiarTexto1 () {
     document.getElementById("loquito").innerHTML = "Sos un puto crack";
     document.getElementById("loquito1").innerHTML = "esto es un block code y va entre {}";
 }
+
