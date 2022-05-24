c0f18612ebaed310cee13cc0d01563d13ba8b2bf
Matías Maranga <marangamatias@gmail.com>
2022-05-24 13:02:09 -0300

    menuDesplegableconJS

--- RCam.js
+++ RCam.js
@@ -7,3 +7,11 @@ function cambiarTexto1 () {
     document.getElementById("loquito1").innerHTML = "esto es un block code y va entre {}";
 }
 
+//MENU DESPLEGABLE CON JAVASCRIPT (Una manera de hacerlo)
+function ver (n) {
+document.getElementById("submenu").style.display="inline-block"
+ }
+function ocultar (n) {
+document.getElementById("submenu").style.display="none"
+}
+
