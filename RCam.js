54aa7cf180514f5bc963ceb90d39280293412dc0
Matías Maranga <marangamatias@gmail.com>
2022-05-27 20:03:02 -0300

    probando_ResponsiveyBurguer

--- RCam.js
+++ RCam.js
@@ -1,17 +1,33 @@
-function cambiarTexto () {
-    document.getElementById ("prueba").innerHTML = "aprendiendo una nueva habilidad";
+function cambiarTexto() {
+  document.getElementById("prueba").innerHTML =
+    "aprendiendo una nueva habilidad";
 }
 
-function cambiarTexto1 () {
-    document.getElementById("loquito").innerHTML = "Sos un puto crack";
-    document.getElementById("loquito1").innerHTML = "esto es un block code y va entre {}";
+function cambiarTexto1() {
+  document.getElementById("loquito").innerHTML = "Sos un puto crack";
+  document.getElementById("loquito1").innerHTML =
+    "esto es un block code y va entre {}";
 }
 
 //MENU DESPLEGABLE CON JAVASCRIPT (Una manera de hacerlo)
-function ver (n) {
-document.getElementById("submenu").style.display="inline-block"
- }
-function ocultar (n) {
-document.getElementById("submenu").style.display="none"
+function ver(n) {
+  document.getElementById("submenu").style.display = "inline-block";
+}
+function ocultar(n) {
+  document.getElementById("submenu").style.display = "none";
 }
 
+var toggleButton = document.getElementById("toggle-icon");
+var sidebar = document.getElementById("sidebar");
+
+console.log(sidebar.classList);
+
+toggleButton.addEventListener("click", function () {
+  if (sidebar.classList.contains("showSidebar")) {
+    sidebar.classList.remove("showSidebar");
+    sidebar.classList.add("hideSidebar");
+  } else {
+    sidebar.classList.add("showSidebar");
+    sidebar.classList.remove("hideSidebar");
+  }
+});
